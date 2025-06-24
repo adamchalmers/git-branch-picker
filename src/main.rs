cacdd30da1ba23350a7ce436c46ec8a5cd455c3c
Adam Chalmers <adam.s.chalmers@gmail.com>
2025-06-23 20:23:25 -0500

    Use ratatui's tabular view

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use anyhow::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
-    buffer::Buffer,
-    layout::Rect,
-    style::Stylize,
-    text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
+    layout::{Constraint, Layout, Margin, Rect},
+    style::{palette::tailwind, Color, Modifier, Style, Stylize},
+    text::Text,
+    widgets::{
+        Block, BorderType, Cell, HighlightSpacing, Paragraph, Row, Scrollbar, ScrollbarOrientation,
+        ScrollbarState, Table, TableState,
+    },
     DefaultTerminal, Frame,
 };
-use std::io;
+
+const ITEM_HEIGHT: usize = 3;
+const PALETTES: [tailwind::Palette; 4] = [
+    tailwind::BLUE,
+    tailwind::EMERALD,
+    tailwind::INDIGO,
+    tailwind::RED,
+];
+const INFO_TEXT: [&str; 1] = ["G'day"];
 
 fn main() -> Result<()> {
     let branches = read_branches()?;
@@ -16,12 +26,19 @@ fn main() -> Result<()> {
     let mut app = App::new(branches)?;
     let app_result = app.run(&mut terminal);
     ratatui::restore();
+    println!("{:#?}", app);
     app_result
 }
 
 #[derive(Debug)]
 struct Branch {
     name: String,
+}
+
+impl Branch {
+    const fn ref_array(&self) -> [&String; 1] {
+        [&self.name]
+    }
 }
 
 fn read_branches() -> anyhow::Result<Vec<Branch>> {
@@ -39,18 +56,64 @@ fn read_branches() -> anyhow::Result<Vec<Branch>> {
 #[derive(Debug)]
 struct App {
     branches: Vec<Branch>,
-    curr: usize,
     exit: bool,
+    state: TableState,
+    scroll_state: ScrollbarState,
+    colors: TableColors,
+    longest_item_lens: u16, // Make this (u16, u16) when I eventually have more fields
+    color_index: usize,
+    checkout_on_exit: bool,
+}
+
+#[derive(Debug)]
+struct TableColors {
+    buffer_bg: Color,
+    header_bg: Color,
+    header_fg: Color,
+    row_fg: Color,
+    selected_row_style_fg: Color,
+    selected_column_style_fg: Color,
+    selected_cell_style_fg: Color,
+    normal_row_color: Color,
+    alt_row_color: Color,
+    footer_border_color: Color,
+}
+
+impl TableColors {
+    const fn new(color: &tailwind::Palette) -> Self {
+        Self {
+            buffer_bg: tailwind::SLATE.c950,
+            header_bg: color.c900,
+            header_fg: tailwind::SLATE.c200,
+            row_fg: tailwind::SLATE.c200,
+            selected_row_style_fg: color.c400,
+            selected_column_style_fg: color.c400,
+            selected_cell_style_fg: color.c600,
+            normal_row_color: tailwind::SLATE.c950,
+            alt_row_color: tailwind::SLATE.c900,
+            footer_border_color: color.c400,
+        }
+    }
 }
 
 impl App {
     fn new(branches: Vec<Branch>) -> Result<Self> {
         Ok(Self {
-            branches,
-            curr: 0,
             exit: false,
+            state: TableState::default().with_selected(0),
+            scroll_state: ScrollbarState::new((branches.len() - 1) * ITEM_HEIGHT),
+            colors: TableColors::new(&PALETTES[1]),
+            color_index: 1,
+            longest_item_lens: constraint_len_calculator(&branches),
+            branches,
+            checkout_on_exit: false,
         })
     }
+
+    fn set_colors(&mut self) {
+        self.colors = TableColors::new(&PALETTES[self.color_index]);
+    }
+
     /// runs the application's main loop until the user quits
     fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
         while !self.exit {
@@ -60,8 +123,15 @@ impl App {
         Ok(())
     }
 
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
+    fn draw(&mut self, frame: &mut Frame) {
+        let vertical = &Layout::vertical([Constraint::Min(5), Constraint::Length(4)]);
+        let rects = vertical.split(frame.area());
+
+        self.set_colors();
+
+        self.render_table(frame, rects[0]);
+        self.render_scrollbar(frame, rects[0]);
+        self.render_footer(frame, rects[1]);
     }
 
     fn handle_events(&mut self) -> Result<()> {
@@ -83,10 +153,10 @@ impl App {
                 self.exit();
             }
             KeyCode::Left | KeyCode::Up | KeyCode::Char('k') | KeyCode::Char('h') => {
-                self.decrement_counter()
+                self.prev_row()
             }
             KeyCode::Right | KeyCode::Down | KeyCode::Char('j') | KeyCode::Char('l') => {
-                self.increment_counter()
+                self.next_row()
             }
             _ => {}
         }
@@ -97,31 +167,128 @@ impl App {
     }
 
     fn switch_branch(&mut self) {
-        // TODO
+        self.checkout_on_exit = true;
     }
 
-    fn increment_counter(&mut self) {
-        self.curr += 1;
+    fn next_row(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.branches.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+        self.scroll_state = self.scroll_state.position(i * ITEM_HEIGHT);
     }
 
-    fn decrement_counter(&mut self) {
-        self.curr -= 1;
+    fn prev_row(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.branches.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+        self.scroll_state = self.scroll_state.position(i * ITEM_HEIGHT);
+    }
+
+    fn render_table(&mut self, frame: &mut Frame, area: Rect) {
+        let header_style = Style::default()
+            .fg(self.colors.header_fg)
+            .bg(self.colors.header_bg);
+        let selected_row_style = Style::default()
+            .add_modifier(Modifier::REVERSED)
+            .fg(self.colors.selected_row_style_fg);
+        let selected_col_style = Style::default().fg(self.colors.selected_column_style_fg);
+        let selected_cell_style = Style::default()
+            .add_modifier(Modifier::REVERSED)
+            .fg(self.colors.selected_cell_style_fg);
+
+        let header = ["Name"]
+            // let header = ["Name", "Address", "Email"]
+            .into_iter()
+            .map(Cell::from)
+            .collect::<Row>()
+            .style(header_style)
+            .height(1);
+        let rows = self.branches.iter().enumerate().map(|(i, data)| {
+            let color = if i % 2 == 0 {
+                self.colors.normal_row_color
+            } else {
+                self.colors.alt_row_color
+            };
+            let item = data.ref_array();
+            item.into_iter()
+                .map(|content| Cell::from(Text::from(format!("\n{content}\n"))))
+                .collect::<Row>()
+                .style(Style::new().fg(self.colors.row_fg).bg(color))
+                .height(ITEM_HEIGHT.try_into().unwrap())
+        });
+        let bar = " > ";
+        let t = Table::new(
+            rows,
+            [
+                // + 1 is for padding.
+                Constraint::Length(self.longest_item_lens + 1),
+                // Constraint::Min(self.longest_item_lens.1 + 1),
+                // Constraint::Min(self.longest_item_lens.2),
+            ],
+        )
+        .header(header)
+        .row_highlight_style(selected_row_style)
+        .column_highlight_style(selected_col_style)
+        .cell_highlight_style(selected_cell_style)
+        .highlight_symbol(Text::from(vec!["".into(), bar.into(), "".into()]))
+        .bg(self.colors.buffer_bg)
+        .highlight_spacing(HighlightSpacing::Always);
+        frame.render_stateful_widget(t, area, &mut self.state);
+    }
+
+    fn render_scrollbar(&mut self, frame: &mut Frame, area: Rect) {
+        frame.render_stateful_widget(
+            Scrollbar::default()
+                .orientation(ScrollbarOrientation::VerticalRight)
+                .begin_symbol(None)
+                .end_symbol(None),
+            area.inner(Margin {
+                vertical: 1,
+                horizontal: 1,
+            }),
+            &mut self.scroll_state,
+        );
+    }
+
+    fn render_footer(&self, frame: &mut Frame, area: Rect) {
+        let info_footer = Paragraph::new(Text::from_iter(INFO_TEXT))
+            .style(
+                Style::new()
+                    .fg(self.colors.row_fg)
+                    .bg(self.colors.buffer_bg),
+            )
+            .centered()
+            .block(
+                Block::bordered()
+                    .border_type(BorderType::Double)
+                    .border_style(Style::new().fg(self.colors.footer_border_color)),
+            );
+        frame.render_widget(info_footer, area);
     }
 }
 
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from("Git branch picker");
-        let block = Block::new().title(title.centered());
-        let lines: Vec<_> = self
-            .branches
-            .iter()
-            .map(|br| Line::from(br.name.clone()))
-            .collect();
-        let counter_text = Text::from(lines);
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf)
-    }
+fn constraint_len_calculator(items: &[Branch]) -> u16 {
+    let name_len = items
+        .iter()
+        .map(|b| b.name.chars().count())
+        .max()
+        .unwrap_or(0);
+
+    name_len as u16
 }
